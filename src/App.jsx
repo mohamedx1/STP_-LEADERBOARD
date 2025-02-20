0597d6badbb2de47638162b2c3fef8343b052e48
mohamedx1 <mohamedalione11@gmail.com>
2025-02-20 02:58:06 +0200

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {Crown} from 'lucide-react';
+import Logo from './assets/LOGO.png'
 
 export default function Leaderboard () {
 
@@ -49,7 +50,8 @@ export default function Leaderboard () {
 
   return (
     <div className="w-full h-max  mx-auto py-8 px-2 md:px-8  bg-[#7d032c]">
-      <div className=" mx-auto p-2 md:p-8 bg-[#fdf6ed]">
+      <img src={Logo} alt='Logo' className='w-28 mb-4 md:w-32' />
+      <div className=" mx-auto p-2 md:p-8 bg-[#fdf6ed] rounded-2xl">
         <h1 className="text-2xl font-medium text-center tracking-[0.3em] mb-8">LEADERBOARD</h1>
         <div className="rounded-lg overflow-hidden ">
           <div className="grid grid-cols-5 text-sm font-medium bg-[#e9dfd4] px-4 py-7">
